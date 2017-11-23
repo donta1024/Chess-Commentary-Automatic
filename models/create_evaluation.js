11d1538eec3597e7ac5ba0a33e13f2d2fcbafb84
AyatsujiP <sample@example.com>
2017-11-24 01:12:10 +0900

    [add]
    add SemanticUI tags to fensubmit

--- models/create_evaluation.js
+++ models/create_evaluation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chessdiagram from 'react-chessdiagram';
+import { Button, Form, TextArea, Container, Grid, Header, Accordion, Icon } from 'semantic-ui-react';
 
 var chess = new Chess();
 
@@ -12,41 +13,59 @@ const squareSize = 45;
 
 class Board extends React.Component{
 	constructor(){
-    super();
-    this.state = {
-      fen: startPosition,
-	  pgn: "",
-	  analyzeResult: analyzeResult,
-	  allowMoves:false
-    };
-  }
+	    super();
+	    this.state = {
+	      fen: startPosition,
+		  pgn: "",
+		  analyzeResult: analyzeResult,
+		  allowMoves:false,
+		  activeIndex: 0 
+	    };
+	}
 	componentWillMount(){
 		this.setState({
 			fen: startPosition,
 			pgn: ""
 		});
-	}
+	};
+
 
 	render() {
+		const panels = [
+			{
+				title: 'Show Details:',
+				content: 'Analyze Result:' +  this.state.analyzeResult
+			}
+		];
+	
 		var self = this;
 		function onMovePiece(piece, fromSquare, toSquare){
 		}
 		
     return (
-			<div>
-    	    	<h3> submit succeeded!</h3>
-				<Chessdiagram
-					allowMoves={this.state.allowMoves}
-					flip={this.props.flip}
-					fen={this.state.fen}
-					squareSize={this.props.squareSize}
-				  lightSquareColor={this.props.lightSquareColor}
-					darkSquareColor={this.props.darkSquareColor}
-					onMovePiece={onMovePiece} />
-				<p>FEN String: {this.state.fen}</p>
-				<p>Analyze Result: {this.state.analyzeResult}</p>
-			</div>
-
+		<div>
+			<Container style={{ marginTop: '3em' }}>
+				<div>
+					<Header as='h1'> submit succeeded!</Header>
+					<Grid columns={2} stackable>
+						<Grid.Column>
+							<Chessdiagram
+								allowMoves={this.state.allowMoves}
+								flip={this.props.flip}
+								fen={this.state.fen}
+								squareSize={this.props.squareSize}
+								lightSquareColor={this.props.lightSquareColor}
+								darkSquareColor={this.props.darkSquareColor}
+							onMovePiece={onMovePiece} />
+						</Grid.Column>
+						<Grid.Column>
+							<p>FEN String: {this.state.fen}</p>
+							<Accordion panels={panels} />
+						</Grid.Column>
+					</Grid>
+				</div>
+			</Container>
+		</div>
     );
   }
 }
